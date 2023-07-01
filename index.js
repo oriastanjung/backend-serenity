099eed4de625d4745cfa884f5e383881cb8f8f02
O. Riastanjung <oriastan999@gmail.com>
2023-07-01 20:52:19 +0700

    finish

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
-const placesRouter = require('./api/places/router');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const placesRouter = require("./api/places/router");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const app = express();
-const cors = require("cors")
-
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 mongoose.set("strictQuery", false);
 
-const connectDB =  async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
-        console.log("Mongodb connected");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Mongodb connected");
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-        process.exit();
-    }
-}
+app.use(
+  cors({
+    origin: true,
+  })
+);
 
-app.use(cors())
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-app.get("/", (req,res) => {
-    return res.send("Welcome to Serenity Free API")
-})
-app.use("/places", placesRouter)
-
+app.get("/", (req, res) => {
+  return res.send("Welcome to Serenity Free API");
+});
+app.use("/places", placesRouter);
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`listening on localhost:${PORT}`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`listening on localhost:${PORT}`);
+  });
+});
